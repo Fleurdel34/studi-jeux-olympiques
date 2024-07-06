53156b67fc9d7accb214d34b9de102f941ecce4f
Fleurdel34 <betdevelweb0953@gmail.com>
2024-07-06 17:12:35 +0200

    Create validator and function to validate the same password on form

--- frontend-jeuxolympiques/frontend-jeuxolympiques/src/app/accountcreation/accountcreation.component.ts
+++ frontend-jeuxolympiques/frontend-jeuxolympiques/src/app/accountcreation/accountcreation.component.ts
@@ -1,26 +1,78 @@
 import {Component, OnInit} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl, FormControlOptions,
+  FormControlState,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators
+} from "@angular/forms";
+import {confirmEqualValidator} from "../validators/confirmEqualValidator";
+import {map, Observable} from "rxjs";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-accountcreation',
   standalone: true,
   imports: [
-    FormsModule
+    ReactiveFormsModule,
+    AsyncPipe,
+    NgIf
   ],
   templateUrl: './accountcreation.component.html',
   styleUrl: './accountcreation.component.css'
 })
-export class AccountcreationComponent {
-    userName!: string;
-    userFirstname!: string;
-    userEmail!:string;
-    userTelephone!:number;
-    userPassword!: string;
-    userPasswordConfirmation!:string;
+export class AccountcreationComponent implements OnInit {
 
-    /*recover all form values*/
-    onSubmitForm(form: NgForm){
-      console.log(form.value);
+  registrationForm!: FormGroup;
+  passwordRegex!: RegExp;
+
+  userPasswordCtrl!: FormControl;
+  userPasswordConfirmationCtrl!: FormControl;
+
+
+  showPasswordError$!: Observable<boolean>
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.passwordRegex = /^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\D*\d).{10,}$/;
+
+    this.userPasswordCtrl =this.formBuilder.control('', [Validators.required, Validators.pattern(this.passwordRegex)]);
+    this.userPasswordConfirmationCtrl =this.formBuilder.control('', [Validators.required, Validators.pattern(this.passwordRegex)]);
+
+    this.registrationForm = this.formBuilder.group({
+      userLastname: [null, [Validators.required]],
+      userFirstname: [null, [Validators.required]],
+      userEmail: [null, [Validators.required, Validators.email]],
+      userTelephone: [null, [Validators.required]],
+      userPassword: this.userPasswordCtrl,
+      userPasswordConfirmation: this.userPasswordConfirmationCtrl
+    },{validators: [confirmEqualValidator('userPassword', 'userPasswordConfirmation')], updateOn: 'blur'});
+
+    this.initFormObservables();
+  }
+
+  /* Create Observable to write comment error*/
+
+  private initFormObservables(){
+    this.showPasswordError$ =this.registrationForm.statusChanges.pipe(
+      map(status => status === 'INVALID' && this.userPasswordCtrl.value && this.userPasswordConfirmationCtrl.value)
+    )
+  }
+
+  /*recover all form values*/
+    onSubmitForm(){
+      if (this.registrationForm.invalid) {
+        alert("La saisie de votre formulaire est erronée ou incomplète!");
+      } else {
+
+        let formValue= this.registrationForm.value;
+        console.log(formValue);
+        this.registrationForm.reset();
+      }
     }
 
 }
