8a78c03d15c0bdfe99d00501dadccdadd14419d2
Fleurdel34 <betdevelweb0953@gmail.com>
2024-07-08 12:32:13 +0200

    udpdate header button and title when page changes

--- frontend-jeuxolympiques/frontend-jeuxolympiques/src/app/header/header.component.ts
+++ frontend-jeuxolympiques/frontend-jeuxolympiques/src/app/header/header.component.ts
@@ -1,26 +1,37 @@
 import {Component, OnInit} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import {NgIf, NgOptimizedImage, NgStyle} from "@angular/common";
 import {Router} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgIf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
 
-  constructor(private router: Router){}
 
-  ngOnInit(): void{}
+  constructor(protected router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
 
   /* Create method for redirect Account Creation Page with route and path*/
-  onRedirectAccountCreation(): void{
-      this.router.navigateByUrl('/registration');
+  onRedirectAccountCreation(): void {
+    this.router.navigateByUrl('/registration');
+  }
+
+  /* Create method for redirect Home Page if page isn't Home*/
+  onRedirectBackHomePage(): void {
+    this.router.navigateByUrl('/');
   }
 
 }
