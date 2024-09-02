7ae84b78f4c89710965f6d4f6407699c9036f477
Fleurdel34 <betdevelweb0953@gmail.com>
2024-09-02 09:40:24 +0200

    begin process environment variable

--- frontend-jeuxolympiques/frontend-jeuxolympiques/src/environments/environments.ts
+++
@@ -1,8 +0,0 @@
-export const environment={
-  production: false,
-  url:'http://localhost:8080/api/users',
-  urlActivation:'http://localhost:8080/api/users/activation',
-  urlConnection:'http://localhost:8080/api/users/connection',
-  urlOffers:'http://localhost:8080/api/offers',
-  urlPayment:'http://localhost:8080/api/payment'
-}
